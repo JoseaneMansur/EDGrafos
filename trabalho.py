df645f903650eb1264dd06f949a56f0477a5097b
Heuller <heullergit@gmail.com>
2018-05-01 11:12:43 -0300

    alteracao no import

--- trabalho.py
+++ trabalho.py
@@ -1,7 +1,7 @@
-import funcoes
+from funcoes import *
 
 nomeArquivo = input("Digite o nome do arquivo: ")
-resultado = funcoes.direcionadoOuNao(nomeArquivo)
+resultado = direcionadoOuNao(nomeArquivo)
 print(resultado)
 
 
