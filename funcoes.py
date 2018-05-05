4d5c22d56f7697ffd4d3f5e3bc6d90e8b2c47278
willdc4 <williamdcoelho@gmail.com>
2018-05-05 17:44:02 -0300

    Listar Vertices/Arestas OK

--- funcoes.py
+++ funcoes.py
@@ -19,27 +19,36 @@ def listarLigacoes(arq):
 	arq = "instances/Padrao_Txt/" + arq
 	manipulador = open(arq,'r')
 	lista = []
+	#Para cada linha do arquivo cria uma lista com os vértices pertencentes
+	#à aresta. Ao final  temos como retorno uma lista de listas 
+	#Cada elemento da lista  final é um par(u,v) representando uma aresta 	
 	for linha in manipulador:
-		linha = linha.rstrip()
-		lista = lista + [linha]
+		lista.append(linha.split())
 	lista.pop(0)
 	manipulador.close()
 	return lista
 
+
+#Função que cria uma lista em que cada elemento dessa lista 
+#é um vértice
 def listarVertices(arq):
 	arq = "instances/Padrao_Txt/" + arq
 	manipulador = open(arq,'r')
 	lista = []
 	for linha in manipulador:
-		linha = linha.rstrip()
-		lista = lista + [linha]
+		lista.append(linha.split())
 	lista.pop(0)
-	lista.split(' ')
-	dicionario = {}
-	for vertice in lista:
-		dicionario[vertice] = vertice
-	lista = dicionario.keys()
-	return lista
+	manipulador.close()
+	listaVertices = []
+	#Percorre cada aresta (par de vértices) da lista inserindo em uma lista
+	# de vertices todos os vertices sem repetição e em ordem crescente 	
+	for par in lista:
+		for elemento in par:
+			if elemento not in listaVertices:
+				listaVertices.append(elemento)
+	listaVertices.sort()
+	
+	return listaVertices
 	
 	
 #n5_dir_unwgt_comb0.txt
