fdb21acc0390198fe34255757dbb53a2ab090797
Heuller <heullergit@gmail.com>
2018-05-05 21:14:01 -0300

    apos o merge

--- trabalho.py
+++ trabalho.py
@@ -3,16 +3,12 @@ from classes import *
 
 nomeArquivo = input("Digite o nome do arquivo: ")
 #resultado = direcionadoOuNao(nomeArquivo)
-<<<<<<< HEAD
 lista = listarLigacoes(nomeArquivo)
 print(lista)
 
 
-
-=======
 listaA = listarLigacoes(nomeArquivo)
 listaV = listarVertices(nomeArquivo)
 grafo = Grafo(listaV,listaA)
 print('Vertices: ', grafo.vertices)
 print('Arestas: ', grafo.arestas)
->>>>>>> origin/v2
